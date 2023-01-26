6bf9a29641f440508be88d3d7ee3477b10617db7
mrgarshin <mrgarshin@yandex.ru>
2023-01-26 11:30:46 +0300

    add: object and function tasks - 70%

---
+++ objAndFunc.js
@@ -0,0 +1,36 @@
+//+Create a car object, add a color property to it with the value equals 'black'
+//+Change the color property of the car object to 'green'
+//+Add the power property to the car object, which is a function and displays the engine power to the console
+//+Pears and apples are accepted to the warehouse, write a function that returns the result of adding the number of accepted pears and apples
+//Your name is saved in the payment terminal, write a function to define the name in the terminal (if you entered your name, then hello + name, if not, then there is no such name)
+//+Write a function for calculating the type of argument and type output to the console
+//Write a function that determines whether a number is prime or not
+
+//create a car object
+const car = {
+  color: 'black',
+}
+
+//change the color to 'green'
+car.color = 'green';
+
+//add the power prop and etc..
+car.power = function(engine) {
+  console.log(`Engine power of this car: ${engine}`)
+}
+
+//Result of adding pears and apples to the warehouse
+const addingFruits = function(pearsCount, applesCount) {
+  return pearsCount+applesCount;
+}
+
+//Name form payment terminal
+
+
+//What type of argument is it?
+const typeOfArg = function(argument) {
+  console.log(typeof(argument));
+}
+
+//Determine number prime or not
+
