f37e4e4344d96e59119bd86bef742ec2da3d66a5
mrgarshin <mrgarshin@yandex.ru>
2023-01-26 11:03:59 +0300

    init: add 75% of datatypes tasks and node_modules

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+// Perform addition of various types (string + boolean, string + number, number + boolean)
+// Perform multiplication of various types (string * boolean, string * number, number * boolean)
+// Divide different types (string / boolean, string / number, number / Boolean)
+// Perform explicit conversion (number, string, boolean)
+let string = "String";
+let boolean = true;
+let number = 2;
+
+//Addition
+console.log(string+boolean);
+console.log(string+number);
+console.log(number+boolean);
+
+//Multiplication
+console.log(string*boolean);
+console.log(string*number);
+console.log(number*boolean);
+
+//Divide
+console.log(string/boolean);
+console.log(string/number);
+console.log(number/boolean);
+
+//Explicit conversion
+console.log();
+console.log();
+console.log();
