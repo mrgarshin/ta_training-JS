9d97b5826fec366b16e27846a1a73cf6bdd5699c
mrgarshin <mrgarshin@yandex.ru>
2023-01-26 17:25:09 +0300

    add: classes file

---
+++ classes.js
@@ -0,0 +1,11 @@
+// Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+class Animal {
+  move() {
+    console.log('I like to move it, move it ...')
+  }
+}
+
+class Cat extends Animal {
+
+}
+
